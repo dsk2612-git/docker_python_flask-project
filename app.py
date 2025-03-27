54fe1f7c86623e0927afefb574c256b0fd4d6a39
Sai Krishna <saiaws2612@gmail.com>
2025-03-27 09:11:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask sample application change made by sai for CD testing"!!"
+    return "Flask sample application change made by sai for CD testing!"
 
 
 if __name__ == "__main__":
